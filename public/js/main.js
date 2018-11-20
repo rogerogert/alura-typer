a6b5ec52f417260de2d0ecc25fee6be8f440b5f3
Rogério Teixeira <rogerioteixeira91@gmail.com>
2018-11-20 15:12:27 -0200

    Aula 2 - Eventos no jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,14 @@ var tamanhoFrase = $('#tamanho-frase');
 
 tamanhoFrase.text(numPalavras);
 
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
+
